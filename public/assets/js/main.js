019617b2f881fbf73d655e7bf815116dc37669e5
Madison <madisonwmoore@gmail.com>
2019-06-02 17:52:58 -0400

    login styling

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -9,10 +9,11 @@ function showSignUpForm(){
     document.getElementById("signup-button").style.display="none";
 }
 
-Array.from(document.getElementsByClassName("form-text")).forEach(function(element){
-element.addEventListener("onfocus",
+Array.from(document.getElementsByClassName("login-input")).forEach(function(element){
+element.addEventListener("focus",
 function (){
 element.value="";
+element.style.color="#B21B00";
 
 });
 
