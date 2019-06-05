c4b4553c74dc4f3e6020b1a5f510a03a7efe1c96
ChrisCogs <ccogs@yahoo.com>
2019-06-05 13:30:27 -0400

    modified models slightly

--- models/quizlist.js
+++ models/quizlist.js
@@ -1,6 +1,12 @@
 module.exports = function(sequelize, DataTypes) {
     var quizList = sequelize.define("Quiz_List", {
-      quiz_name: DataTypes.STRING,
+      quiz_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
       user: DataTypes.JSON
     });
     return quizList;
--- models/user.js
+++ models/user.js
@@ -1,6 +1,12 @@
 module.exports = function(sequelize, DataTypes) {
     var User = sequelize.define("User", {
-      user_name: DataTypes.STRING,
+      user_name:{
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
       quiz_list: DataTypes.STRING
     });
     return User;
