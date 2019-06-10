fa075ab744833f256d384d83198a9e7e6e6c09bc
Madison <madisonwmoore@gmail.com>
2019-06-10 18:23:08 -0400

    stash

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
 var express = require("express");
-
 var PORT = process.env.PORT || 3000;
-
 var db = require("./models");
-
 var app = express();
 var Handlebars=require("handlebars");
+var exphbs = require("express-handlebars");
 
 app.use(express.static("public"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
@@ -22,20 +17,17 @@ var apiroutes=require("./routes/api-routes.js");
 var dashboardroutes =require("./routes/dashboard-routes.js");
 var loginroutes=require("./routes/login-routes.js");
 
-
 app.use(htmlroutes);
 app.use(apiroutes);
 app.use(dashboardroutes);
 app.use(loginroutes);
 
-db.sequelize.sync().then(function() {
+db.sequelize.sync({force:true}).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
 });
 
-
-// 
 Handlebars.registerHelper("equals", function(string1 ,string2, options) {
   if (string1 === string2) {
       return options.fn(this);
