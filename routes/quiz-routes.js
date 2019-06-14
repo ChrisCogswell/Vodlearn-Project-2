6a1c204f3ca2a6495e85e87f631edb53d9e28dbd
ChrisCogs <ccogs@yahoo.com>
2019-06-14 17:37:23 -0400

    fixed quiz-routes, working on quiz handlebars

--- routes/quiz-routes.js
+++ routes/quiz-routes.js
@@ -2,19 +2,27 @@ var express = require("express");
 var router = express.Router();
 var db = require("../models");
 
-// router.get("/quiz/:id", (req,res)=>{
-// //Sequelize
-// db.Question.findAll({where:{QuizId:req.params.id}}).then(function(results){
-//     res.render("quiz-stage",{layout: false, view:"quiz-body", question:results});
+router.get("/quiz/:id", (req,res)=>{
+//Sequelize
+db.Question.findAll({where:{QuizId:req.params.id}}).then(function(result1){
+    db.Quiz.findAll({where:{id:req.params.id}}).then(function(result2){
+    res.render("quiz", {
+      question: result1,
+      quiz: result2
+    });
+  });
+  });
+});
+
+// router.get("/quiz/:id", (req,res) => {
+
+//   db.Quiz.findAll({where:{id:req.params.id}}).then(function(results){
+//     res.render("quiz-stage",{layout: false, view:"quiz-header", quiz:results});
 //   })
 // })
 
-router.get("/quiz/:id", (req,res) => {
-
-  db.Quiz.findAll({where:{id:req.params.id}}).then(function(results){
-    res.render("quiz-stage",{layout: false, view:"quiz-header", quiz:results});
-  })
-})
-
 
 module.exports = router;
+
+
+
